8c22e4c2c701cbe0e90c7b033851c061b3dd9682
Chiroxx <101751087+Chiroxx@users.noreply.github.com>
2026-09-12 06:29:52 +0200

    Sync: Erst laden, dann speichern - kein race condition mehr

--- supabase-config.js
+++ supabase-config.js
@@ -226,14 +226,12 @@
                 loadAllFromDB(function() {
                     console.log('[DB] Alle Daten aus der Datenbank geladen!');
                     setupDBLogin();
-                    // Alle 30 Sekunden: Daten AUS DB laden (fuer Kollegen)
+                    // Alle 30 Sekunden: Erst AUS DB laden, dann IN DB speichern
                     setInterval(function() {
                         loadAllFromDB(function() {
-                            console.log('[DB] Daten aktualisiert!');
+                            saveAllToDB();
                         });
                     }, 30000);
-                    // Alle 60 Sekunden: Daten IN DB speichern
-                    setInterval(saveAllToDB, 60000);
                 });
             });
         } catch(e) {
